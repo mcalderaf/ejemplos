138bec930ebefcc185f0fdd5de3c8223736679b6
mcalderaf <mcaldera.tec2@gmail.com>
2023-07-27 20:48:15 -0600

    clase 27 julio

---
+++ api/main.py
@@ -0,0 +1,11 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+# creamos una ruta para acceder a un cierrto valor
+@app.get("/")
+def raiz():
+    return {"Hello": "Word"}
+
+@app.get("/items/{item_id}/{m}")
+def read_item(item_id: int, m: str = None):
+    return {"item_id": item_id, "m": m}
